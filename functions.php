0741e59d299e216da5f1c1a93150b91b3123c11e
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-01-06 05:48:15 +0000

    Remove custom header

--- functions.php
+++ functions.php
@@ -143,10 +143,6 @@ function hoangvvo_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'hoangvvo_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
