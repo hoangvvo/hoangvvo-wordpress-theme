10baa2dc748d2828cec65b7ea7a90d25ca5703df
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-01-08 08:40:03 +0000

    Add container to post navigator

--- single.php
+++ single.php
@@ -18,9 +18,13 @@ get_header();
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
-
+		?>
+			<div class="container">
+		<?php
 			the_post_navigation();
-
+		?>
+			</div>
+		<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
