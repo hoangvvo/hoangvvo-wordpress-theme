10f9e59f85aeb9b754628569ab9d62fd1a6490ca
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-03-31 16:17:17 +0000

    Lib merge + CDN usage

--- js/util.js
+++ js/util.js
@@ -1,1 +1,4 @@
+//animateView
+$(document).on("scroll",function(){for(var o=$(document).scrollTop()+$(window).height(),i=$(".animateView"),e=0;e<i.length;e++){var l=i[e];$(l).position().top<o?$(l).delay(1000).addClass("visible"):$(l).removeClass("visible")}});
+//smooth scroll
 $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);(e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top},1e3,function(){var t=$(e);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}});
