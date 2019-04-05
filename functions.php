95ad608a13155908cbfd3b987cef88bc07599b76
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-04-05 14:53:57 +0000

    fix typo

--- functions.php
+++ functions.php
@@ -122,10 +122,10 @@ add_action( 'widgets_init', 'hoangvvo_widgets_init' );
 function hoangvvo_scripts() {
 	wp_deregister_script('jquery');//remove default jquery
 	wp_deregister_script( 'wp-embed' ); //remove embed
-	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '', true);
-	wp_enqueue_script( 'bootstrapCss', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), '', true);
-	wp_enqueue_script( 'bootstrapJs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array(), '', true);
+	wp_enqueue_style( 'bootstrapCss', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), '', true);
 	wp_enqueue_style( 'hoangvvo-style', get_stylesheet_uri(), array(), '1.0.2');
+	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '', true);
+	wp_enqueue_script( 'bootstrapJs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array(), '', true);
 	wp_enqueue_script( 'hoangvvo-util', get_template_directory_uri() . '/js/util.js', array(), '1.0.2', true);
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
