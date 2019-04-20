5f17c1af6e55fe11c2e72c99f28c455cd3b7e6a0
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-04-20 15:42:29 +0000

    clean up

--- single.php
+++ single.php
@@ -19,11 +19,9 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 		?>
-			<div class="container">
 		<?php
-			the_post_navigation();
+			//the_post_navigation();
 		?>
-			</div>
 		<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
